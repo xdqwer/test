ba2934c90ee0b73be0b0cac72ee33d2525bb31e2
dxm <1728900192@qq.com>
2023-04-03 23:25:53 +0800

    first commit

---
+++ src/main/java/com/dxm/student/dxm/DxmApplication.java
@@ -0,0 +1,13 @@
+package com.dxm.student.dxm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DxmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DxmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dxm/student/dxm/DxmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dxm.student.dxm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DxmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
